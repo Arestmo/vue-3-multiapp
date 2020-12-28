ee15a358d223df879778204c6db588088336071a
MateuszT <mateusz@clearmedia.pl>
2020-12-28 12:55:18 +0100

    New component has been added, simplification of header menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,24 +1,42 @@
 <template>
   <nav class="w-full bg-gradient-to-r from-blue-800 to-blue-600 text-white py-4 px-4">
-    <router-link to="/list-app" class="header-item px-2 py-3 mx-3">List App</router-link>
-    <router-link to="/calendar-app" class="header-item px-2 py-3 mx-3">Calendar App</router-link>
+    <router-link v-for="item in items" :key="item.to" :to=item.to class="header-item px-2 py-3 mx-3">{{ item.title }}</router-link>
   </nav>
 </template>
 
 <script>
 export default {
-name: "Header"
+  name: "Header",
+  data() {
+    return {
+      items: [
+        {
+          title: 'List App',
+          to: '/list-app'
+        },
+        {
+          title: 'Calendar App',
+          to: '/calendar-app'
+        },
+        {
+          title: 'Markdown App',
+          to: '/markdown-app'
+        },
+      ]
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-  .header-item {
-    border: 1px solid white;
-    opacity: .5;
-    transition: .3s opacity ease-in-out;
-    &:hover {
-      opacity: 1;
-    }
+.header-item {
+  border: 1px solid white;
+  opacity: .5;
+  transition: .3s opacity ease-in-out;
+
+  &:hover {
+    opacity: 1;
   }
+}
 
 </style>
--- src/router.js
+++ src/router.js
@@ -1,10 +1,12 @@
 import {createRouter, createWebHistory} from 'vue-router';
 import Home from "@/components/Home";
 import Calendar from "@/components/Calendar";
+import Markdown from "@/components/Markdown";
 const routes = [
   {path: '/', component: Home},
   {path: '/list-app', component: Home},
-  {path: '/calendar-app', component: Calendar}
+  {path: '/calendar-app', component: Calendar},
+  {path: '/markdown-app', component: Markdown},
 ]
 const router = createRouter({
   history: createWebHistory(),
