c1b77f493820b30a573660b1c1065912a23ee0a0
MateuszT <mateusz@clearmedia.pl>
2020-12-28 15:57:19 +0100

    Add markdown functionality

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -1,12 +1,40 @@
 <template>
-  <div>
-    <h1>Markdown</h1>
+  <div class="flex flex-wrap w-full">
+    <h1 class="w-full text-center text-3xl my-4">Markdown APP</h1>
+    <section class="flex m-auto w-10/12 h-screen">
+      <article class="w-1/2 border p-2">
+        <textarea class="w-full h-full outline-none" :value="text" @input="updateText"></textarea>
+      </article>
+      <article class="w-1/2 border p-2 bg-gray-100" v-html="markedText"></article>
+    </section>
   </div>
 </template>
-
 <script>
+import marked from 'marked';
+
 export default {
-  name: "Markdown"
+  name: "Markdown",
+  data() {
+    return {
+      text: '',
+      timeout: '',
+    }
+  },
+  computed: {
+    markedText() {
+      return marked(this.text);
+    }
+  },
+  methods: {
+    updateText(e) {
+      const task = () => (this.text = e.target.value);
+      this.debounce(task, 500);
+    },
+    debounce(func, wait = 1000) {
+      clearTimeout(this.timeout);
+      this.timeout = setTimeout(func, wait)
+    },
+  }
 }
 </script>
 
