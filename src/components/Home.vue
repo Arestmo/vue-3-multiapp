6e612d6a78500c341bc543bc7864c6f789a591a7
MateuszT <mateusz@clearmedia.pl>
2021-01-04 11:39:01 +0100

    Add composition API

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,55 +2,69 @@
   <div class="w-full flex">
     <div class="m-auto">
       <h1 class="text-2xl text-center">
-        {{ herosCount }} Marvel Heros
+        {{ itemsCount }} Items
       </h1>
       <ul>
-        <li class="flex justify-between" v-for="(hero,id) in herosList" :key="id">
-          {{ hero.name }}
-          <button @click="removeHero(id)">x</button>
+        <li class="flex justify-between" v-for="(item,id) in listOfItems" :key="id">
+          {{ item.name }}
+          <button class="outline-none p-2" @click="removeHero(id)">x</button>
         </li>
       </ul>
-      <input class="border rounded mt-10" placeholder="Type hero name" v-model.trim="newHero">
+      <input class="border rounded mt-10"
+             placeholder="Type hero name"
+             v-model.trim="newItem"
+             ref="newItemRef"
+      >
       <button class="border rounded bg-gradient-to-r from-red-700 to-pink-500 text-sm text-white p-2"
-              @click="addHero">Add Hero</button>
+              @click="addHero">Add Hero
+      </button>
     </div>
   </div>
 </template>
 
 <script>
+import {computed, onMounted, ref} from "vue";
+
 export default {
   name: "Home",
-  data() {
-    return {
-      newHero: "",
-      herosList: [
-        {
-          name: "IronMan"
-        },
-        {
-          name: "Capitan America"
-        },
-        {
-          name: "SpiderMan"
-        },
-      ]
-    };
-  },
-  computed: {
-    herosCount() {
-      return this.herosList.length;
+  setup() {
+    const newItemRef = ref("");
+    const newItem = ref("");
+    const listOfItems = ref([
+      {name: "Item 1"},
+      {name: "Item 2"},
+      {name: "Item 3"},
+    ]);
+
+    onMounted(() => {
+      newItemRef.value.focus();
+    });
+
+    const itemsCount = computed({
+      get: () => listOfItems.value.length,
+    })
+
+    function removeHero(id) {
+      listOfItems.value.splice(id, 1)
     }
-  },
-  methods: {
-    addHero() {
-      if (this.newHero.length > 0) {
-        this.herosList.push({name: this.newHero})
-        this.newHero = '';
+
+    function addHero() {
+      if (newItem.value.length > 0) {
+        listOfItems.value.push({name: newItem.value})
+        newItem.value = "";
       }
-    },
-    removeHero(id) {
-      this.herosList.splice(id, 1)
     }
+
+
+    return {
+      listOfItems,
+      newItem,
+      removeHero,
+      addHero,
+      newItemRef,
+      itemsCount
+    }
+
   }
 }
 </script>
